009be98adb0f75f21b9e12130550e6e2116b74bc
Rickyflame <kimumuricky@gmail.com>
2025-09-26 22:05:45 +0300

    restart

--- racelogic-backend/src/race/race.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { RaceController } from './race.controller';
-import { RaceService } from './race.service';
-
-@Module({
-  controllers: [RaceController],
-  providers: [RaceService],
-})
-export class RaceModule {}
